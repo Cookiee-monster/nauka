b82c314ff7036c466acec5cf31445fcf8b02d1d2
Cookies-monster <grzegorzkuprewicz@gmail.com>
2019-05-26 11:55:48 +0200

    complete

--- Scripts/Exercism/python/isbn-verifier/isbn_verifier.py
+++ Scripts/Exercism/python/isbn-verifier/isbn_verifier.py
@@ -1,5 +1,20 @@
 import re
 
 def verify(isbn):
-    re.findall("([0-9])")
-    pass
+    isbn_as_a_list = re.findall("([0-9X])", isbn)
+    if len(isbn_as_a_list) != 10:
+        return False
+    result = 0
+    multiplier = 10
+    for digit in isbn_as_a_list:
+        if digit.isdigit():
+            result += int(digit) * multiplier
+        elif digit == "X" and multiplier == 1:
+            result += 10 * multiplier
+        else:
+            return False
+        multiplier -= 1
+    if result % 11 == 0:
+        return True
+    else:
+        return False

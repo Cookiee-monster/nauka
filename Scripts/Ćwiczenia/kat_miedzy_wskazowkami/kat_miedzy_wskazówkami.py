b43bbc80848844d6a8210cbfba43f6870cbdfefc
Cookies-monster <grzegorzkuprewicz@gmail.com>
2019-06-16 11:49:32 +0200

    relocation

---
+++ Scripts/Ćwiczenia/kat_miedzy_wskazowkami/kat_miedzy_wskazówkami.py
@@ -0,0 +1,27 @@
+def input_data(tekst):
+    return input(tekst)
+
+
+def kat_godziny(godzina, minuta=0):
+    if godzina <= 12:
+        return godzina * 30 + minuta * 0.5
+    elif godzina <= 24:
+        return (godzina - 12 ) * 30 + minuta * 0.5
+    else:
+        raise ValueError
+
+
+def kat_minuty(minuta):
+    return minuta * 6
+
+
+def podaj_kat():
+    godzina = int(input_data("Podaj godzinę"))
+    minuta = int(input_data("Podaj minutę"))
+
+    kat_godzinowy = kat_godziny(godzina, minuta)
+    kat_minutowy = kat_minuty(minuta)
+
+    kat = abs(kat_godzinowy - kat_minutowy)
+    print("Kąt pomiędzy wskazówkami dla godziny {}:{} wynosi {} stopni".format(godzina, minuta, kat))
+    return kat
---
+++ Scripts/Ćwiczenia/kat_miedzy_wskazowkami/test_kat_miedzy_wskazowkami.py
@@ -0,0 +1,17 @@
+from kat_miedzy_wskazówkami import input_data, kat_godziny, kat_minuty
+
+import unittest
+
+class KatTest(unittest.TestCase):
+    def test_kat_godziny_12(self):
+        self.assertEqual(kat_godziny(12), 360)
+
+    def test_kat_godziny_15_15(self):
+        self.assertEqual(kat_godziny(15,15), 97.5)
+
+    def test_kat_godziny_26_00(self):
+        kat_godziny(26)
+        self.assertRaises(ValueError)
+
+if __name__ == "__main__":
+    unittest.main()
